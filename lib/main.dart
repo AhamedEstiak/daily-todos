64db805af1f563594791ba1cc76673b61eb902ca
estiak <aiubpolin@gmail.com>
2023-07-24 07:44:54 +0600

    imports from all model has been update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:daily_todos/screens/home/splash_sreen.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+
 import 'package:daily_todos/utils/theme_util.dart';
 import 'package:daily_todos/config/provider_config.dart';
-import 'package:daily_todos/model/global_model.dart';
+import 'package:daily_todos/model/all_model.dart';
+import 'package:daily_todos/screens/home/splash_sreen.dart';
 
 void main() {
   runApp(
